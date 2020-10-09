eb66f6914fb3afaa278293e22ef4311b35bf0a2f
wanghongbo <286892469@qq.com>
2020-10-09 10:34:50 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Hello :msg="$store.state.demo.msg" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Hello from "@/components/Hello.vue";
+import { getList } from "@/api/demo";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Hello
+  },
+  async mounted() {
+    const data = await getList({ f: "ff" });
+    console.log(data);
   }
 };
 </script>
+
+<style scoped lang="less"></style>
