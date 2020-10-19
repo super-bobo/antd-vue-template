0b23175e45fec1ec244a2b8950f35bea8807bbb3
super-bobo <286892469@qq.com>
2020-10-19 16:01:20 +0800

    Update request.js

--- src/api/request.js
+++ src/api/request.js
@@ -2,17 +2,26 @@ import axios from 'axios'
 import { TIMEOUT, API_HOST } from './config'
 import { message } from 'ant-design-vue'
 
+json: 'application/json; charset=utf-8'
+form: 'application/x-www-form-urlencoded; charset=utf-8'
 const request = axios.create({
   baseURL: API_HOST,
   timeout: TIMEOUT,
+  // withCredentials: true,
   headers: {
+    Accept: 'application/json',
     'Content-Type': 'application/json;charset=utf-8'
   }
 })
 
-// request interceptor
+// 请求拦截
 request.interceptors.request.use(
   config => {
+    console.log(config)
+    if (config.tableLoading) {
+      store.commit('common/toggleTableLoading', true)
+    }
+    console.log(store)
     return config
   },
   error => {
@@ -20,16 +29,32 @@ request.interceptors.request.use(
   }
 )
 
-// response interceptor
+// 响应拦截
 request.interceptors.response.use(
   response => {
-    const res = response.data
-
-    if (res.code !== 200) {
-      message.error(res.message || 'Error')
-      return Promise.reject(new Error(res.message || 'Error'))
+    const { data: res, config } = response
+    if (config.tableLoading) {
+      store.commit('common/toggleTableLoading', false)
+    }
+    // code = 0 成功
+    if (res.code === 0) {
+      return Promise.resolve(res.data)
     } else {
-      return res
+      switch (res.code) {
+        case 401: // 未登录
+        case 403: // 没权限
+          router.replace({
+            name: 'login'
+            // query: {
+            //   redirectUrl: router.history.current.fullPath
+            // }
+          })
+          break
+        default:
+          message.error(res.msg || 'Error')
+          break
+      }
+      return Promise.reject(res.msg || 'Error')
     }
   },
   error => {
@@ -41,8 +66,11 @@ request.interceptors.response.use(
 export const get = (url, params, options) => {
   return request({
     url,
-    params,
-    options
+    data: {
+      t: Date.now(),
+      ...params
+    },
+    ...options
   })
 }
 
@@ -51,6 +79,6 @@ export const post = (url, data, options) => {
     method: 'post',
     url,
     data,
-    options
+    ...options
   })
 }
